f8f41176789c3cc15b52a2d801b7379aa7bb924d
Daphnalex <dady.luthien@hotmail.fr>
2016-10-19 15:20:08 +0200

    + ajout filtres

--- JS/old.js
+++ JS/old.js
@@ -2,19 +2,9 @@
 	var app = angular.module('portfolio', []);
 
 	app.controller('portfolioCtrl', function(){
-		this.filtres = [
-			{
-				name: 'all'
-			},
-			{
-				name: 'book'
-			},
-			{
-				name: 'publicite'
-			}
-		];
 		this.elements = [
 			{
+				id: 0,
 				imageMin: '../../IMG/portfolio/image1.png',
 				media: '../../IMG/portfolio/image1.png',
 				type: 'image',
@@ -22,6 +12,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 1,
 				imageMin: '../../IMG/portfolio/image2.png',
 				media: '../../IMG/portfolio/image2.png',
 				type: 'image',
@@ -29,6 +20,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 2,
 				imageMin: '../../IMG/portfolio/image3.png',
 				media: '../../IMG/portfolio/image3.png',
 				type: 'image',
@@ -36,6 +28,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 3,
 				imageMin: '../../IMG/portfolio/image4.png',
 				media: '../../IMG/portfolio/image4.png',
 				type: 'image',
@@ -43,6 +36,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 4,
 				imageMin: '../../IMG/portfolio/image5.png',
 				media: '../../IMG/portfolio/image5.png',
 				type: 'image',
@@ -50,6 +44,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 5,
 				imageMin: '../../IMG/portfolio/image6.png',
 				media: '../../IMG/portfolio/image6.png',
 				type: 'image',
@@ -57,13 +52,15 @@
 				filtre: 'book'
 			},
 			{
+				id: 6,
 				imageMin: '../../IMG/portfolio/image7.png',
 				media: '../../IMG/portfolio/image7.png',
 				type: 'image',
 				description : 'We are team of creative photographers. We passionate with photography and other creative things. If you are looking professional photography theme with endless possibilities, you come in right place. This template consist of well-organized layers. Tons of features waiting for you.',
-				filtre: 'book'
+				filtre: 'publicite'
 			},
 			{
+				id: 7,
 				imageMin: '../../IMG/portfolio/video1.png',
 				media: '../../VIDEO/portfolio/video1.mp4',
 				type: 'video',
@@ -71,6 +68,7 @@
 				filtre: 'publicite'
 			},
 			{
+				id: 8,
 				imageMin: '../../IMG/portfolio/image1.png',
 				media: '../../IMG/portfolio/image1.png',
 				type: 'image',
@@ -78,6 +76,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 9,
 				imageMin: '../../IMG/portfolio/image2.png',
 				media: '../../IMG/portfolio/image2.png',
 				type: 'image',
@@ -85,6 +84,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 10,
 				imageMin: '../../IMG/portfolio/image3.png',
 				media: '../../IMG/portfolio/image3.png',
 				type: 'image',
@@ -92,6 +92,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 11,
 				imageMin: '../../IMG/portfolio/image4.png',
 				media: '../../IMG/portfolio/image4.png',
 				type: 'image',
@@ -99,6 +100,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 12,
 				imageMin: '../../IMG/portfolio/image5.png',
 				media: '../../IMG/portfolio/image5.png',
 				type: 'image',
@@ -106,6 +108,7 @@
 				filtre: 'book'
 			},
 			{
+				id: 13,
 				imageMin: '../../IMG/portfolio/image6.png',
 				media: '../../IMG/portfolio/image6.png',
 				type: 'image',
@@ -113,13 +116,15 @@
 				filtre: 'book'
 			},
 			{
+				id: 14,
 				imageMin: '../../IMG/portfolio/image7.png',
 				media: '../../IMG/portfolio/image7.png',
 				type: 'image',
 				description : 'We are team of creative photographers. We passionate with photography and other creative things. If you are looking professional photography theme with endless possibilities, you come in right place. This template consist of well-organized layers. Tons of features waiting for you.',
-				filtre: 'book'
+				filtre: 'publicite'
 			},
 			{
+				id: 15,
 				imageMin: '../../IMG/portfolio/video1.png',
 				media: '../../VIDEO/portfolio/video1.mp4',
 				type: 'video',
@@ -129,60 +134,12 @@
 		];
 
 		this.photoSelectionne = null;
-		this.selectionPhoto = function(element, index, tableau){
+		this.showDiv = false;
+		this.selectionPhoto = function(element, index){
 			this.photoSelectionne = element;
-			this.elements = tableau;
-			var longTab = tableau.length;
-			//this.showDiv = true;
-			let i = ((index - (index%6))/ 6);
-			let max = Math.trunc(longTab/6);
-			//console.log(index);
-			//console.log(longTab - (longTab%6));
-			//console.log(longTab);
-			//console.log(i);
-			//console.log(max);
-			if (i < 1){
-				showDiv=false;
-				angular.element(".show").removeClass("show");
-				angular.element("#5").addClass("show");
-			}
-			else if ((i >= 1) && (i != max)){
-				let id = 6*(Math.trunc(i)+1)-1;
-				angular.element(".show").removeClass("show");
-				angular.element('#'+id).addClass("show");
-			}
-			else if (i=max){
-				let id = longTab-1;
-				//console.log(id);
-				//console.log(id);
-				angular.element(".show").removeClass("show");
-				angular.element('#'+id).addClass("show");
-			}
-			this.fermetureBloc = function(){
-				angular.element(".show").removeClass("show");
-			};
-		}
-		this.filtreSelectionne = null;
-		this.triPhoto = function(filtre, tableauPhoto){
-			this.elements = tableauPhoto ;
-			this.filtreSelectionne = filtre;
-			console.log(filtre);
-			for (i=0; i<tableauPhoto.length; i++){
-				if (filtre.name == "all"){
-					angular.element(".displayNone").removeClass("displayNone");
-				}
-				else if (filtre.name != tableauPhoto[i].filtre){
-					let filtreCache = tableauPhoto[i].filtre;
-					angular.element(".displayNone").removeClass("displayNone");
-					angular.element('.'+ filtreCache).addClass('displayNone');
-				}
-			}
+			this.showDiv = true;
 			
-		}
-
-		
-		
-
+		};
 
 		
 
