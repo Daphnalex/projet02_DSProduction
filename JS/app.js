77ab1bfbf02dceb1749715c6db79c0c7479145aa
Lisa-Stina <liam.stina@gmail.com>
2016-10-20 11:29:47 +0200

    + ajout copyright + index html

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,29 @@
+(function(){
+	
+
+var app = angular.module('dsProd', []);
+	app.controller('dateController', function(){
+			this.date = new Date();
+		
+	});
+
+
+
 app.directive("navBar", function(){
 	return{
-		restrict: 'E,A',
+		restrict: 'EA',
 		templateUrl: "partials/common/nav.html"
 	}
 });
 
 app.directive("piedPage", function(){
 	return{
-		restrict: 'E,A',
+		restrict: 'EA',
 		templateUrl: "partials/common/footer.html"
 	}
 });
+
+
+})();
+
+
