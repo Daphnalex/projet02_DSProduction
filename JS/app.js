540bc446629a27348c5c3b3f75a5bdb142538934
Clément Debroize <clement.debroize@orange.fr>
2016-10-25 14:24:58 +0200

    commit pas du tout atomique de 14:24

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,48 @@
 (function(){
-	
 
-var app = angular.module('dsProd', []);
+	var app = angular.module('dsProd', []);
 
 //Mise à jour copyright 
-	app.controller('dateController', function(){
-			this.date = new Date();
-		
-	});
+app.controller('dateController', function(){
+	this.date = new Date();
+
+});
+
+
+
+app.directive("bloc1", function(){
+	return{
+		restrict: 'EA',
+		name: 'bloc1',
+		templateUrl: "/partials/services/bloc1.html"
+	}
+})
+
+app.directive("bloc2", function(){
+	return{
+		restrict: 'EA',
+		name: 'bloc2',
+		templateUrl: "/partials/services/bloc2.html"
+	}
+})
+
+
+app.directive("bloc3", function(){
+	return{
+		restrict: 'EA',
+		name: 'bloc3',
+		templateUrl: "/partials/services/bloc3.html"
+	}
+})
+
+
+app.directive("bloc4", function(){
+	return{
+		restrict: 'EA',
+		name: 'bloc4',
+		templateUrl: "/partials/services/bloc4.html"
+	}
+})
 
 
 
@@ -28,4 +63,6 @@ app.directive("piedPage", function(){
 
 })();
 
-
+app.controller('ServController', function(){
+	
+});	
